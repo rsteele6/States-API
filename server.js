0042cbb61661a116c5d499cc01e41c110eb2f15e
rsteele6 <rsteele6@gmail.com>
2023-07-04 13:35:34 -0400

    updated packages

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const cors = require('cors');
 // const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/dbConn');
 
-// const PORT = process.env.PORT || 3500;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3500;
 
 // Cross Origin Resource Sharing
 //app.use(cors(corsOptions));
